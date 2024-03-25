0136ec8fb34584a40b3b53d358cbd40d9af25c89
Vaishnavi Shukla <vaishnavi14shukla@gmail.com>
2024-03-25 18:53:40 +0530

    commit on 25-03-2024
    commit on 25-03-2024

--- Frontend/src/MandatoryItems/SubAppBar.js
+++ Frontend/src/MandatoryItems/SubAppBar.js
@@ -13,26 +13,26 @@ export default function ButtonAppBar() {
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <div className="row">
-         <div className="col-sm-1 headingone">
-          <h3 style={{marginLeft:'50px'}}><Link to="/NationalNews.js" style={{textDecoration:'none',color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman'}}>NATIONAL</Link></h3>
-          </div>
-
-          <div className="col-sm-2 heading">
-          <h3 style={{marginLeft:'195px'}}><Link to="/Internationalnews.js" style={{textDecoration:'none',color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman'}}>INTERNATIONAL</Link></h3>
+         <div className="col-sm-3 heading">
+          <h3 style={{marginLeft:'5px'}}><Link to="/nationalnews" style={{textDecoration:'none',color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman'}}>NATIONAL</Link></h3>
           </div>
 
           <div className="col-sm-3 heading">
-          <h3 style={{marginLeft:'310px'}}><Link to="/Sportsnews.js" style={{textDecoration:'none',color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman'}}>SPORTS</Link></h3>
+          <h3 style={{marginLeft:'5px'}}><Link to="/internationalnews" style={{textDecoration:'none',color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman'}}>INTERNATIONAL</Link></h3>
           </div>
 
-
           <div className="col-sm-3 heading">
-          <h3 style={{marginLeft:'210px'}}><Link to="/Blog.js" style={{color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman',textDecoration:'none'}}>BLOGS</Link></h3>
+          <h3 style={{marginLeft:'5px'}}><Link to="/sportsnews" style={{textDecoration:'none',color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman'}}>SPORTS</Link></h3>
           </div>
 
 
           <div className="col-sm-3 heading">
-          <h3 style={{marginLeft:'70px'}}><Link to="/Educationalnews.js" style={{textDecoration:'none',color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman'}}>EDUCATION CORNER</Link></h3>
+          <h3 style={{marginLeft:'5px'}}><Link to="/blog" style={{color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman',textDecoration:'none'}}>BLOGS</Link></h3>
+          </div>
+
+
+          <div className="col-sm-3 heading">
+          <h3 style={{marginLeft:'5px'}}><Link to="/educationalnews" style={{textDecoration:'none',color:'#000',fontSize:'20px',fontWeight:'bold',fontFamily:'Times New Roman'}}>EDUCATION CORNER</Link></h3>
           </div>
 
           </div>
